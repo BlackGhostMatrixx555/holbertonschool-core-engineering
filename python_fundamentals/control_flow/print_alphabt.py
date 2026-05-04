e862ecf1c4d90e13670e9616f9dbb45d251c15f8
BlackGhostMatrixx555 <thelyaan444@hotmail.com>
2026-05-04 12:01:30 +0200

    Fixing print output command

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 for i in range(ord('a'), ord('z') + 1):
     if chr(i) != 'q' and chr(i) != 'e':
-        print(chr(i), end='' if i != ord('z') else '\n')
+        print("{}".format(chr(i)), end='' if i != ord('z') else '\n')
