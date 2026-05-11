a3dc489846e21b6fba75122c2afedd3f87cb0671
BlackGhostMatrixx555 <thelyaan444@hotmail.com>
2026-05-11 11:28:58 +0200

    Fixing Correct output - case: 98

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -5,5 +5,5 @@
 def print_last_digit(number):
     """Print and return the last digit of number (always positive)."""
     last = abs(number) % 10
-    print("{}".format(last))
+    print("{}".format(last), end='')
     return last
