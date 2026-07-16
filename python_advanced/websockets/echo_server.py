c56c871dcd654066c8ca3f697b4fc608734f282f
BlackGhostMatrixx555 <thelyaan444@hotmail.com>
2026-07-16 12:09:48 +0200

    Fixing PEP8 validation

--- python_advanced/websockets/echo_server.py
+++ python_advanced/websockets/echo_server.py
@@ -5,15 +5,18 @@ WebSocket echo server.
 import asyncio
 import websockets
 
+
 async def connection_handler(websocket):
     """Handler for WebSocket connections."""
     async for message in websocket:
         await websocket.send(message)
 
+
 async def main():
     """Starts the WebSocket server."""
     async with websockets.serve(connection_handler, "localhost", 8765):
         await asyncio.Future()  # Run forever
+
 
 if __name__ == "__main__":
     asyncio.run(main())
