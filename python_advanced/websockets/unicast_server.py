1b90875c507e36b7683d9cf3da88a0e32ce8e67f
BlackGhostMatrixx555 <thelyaan444@hotmail.com>
2026-07-16 12:37:36 +0200

    Fixing PEP8 validation

--- python_advanced/websockets/unicast_server.py
+++ python_advanced/websockets/unicast_server.py
@@ -8,6 +8,7 @@ from websockets.exceptions import ConnectionClosed
 
 connected_clients = set()
 
+
 async def connection_handler(websocket):
     """Handler for WebSocket connections with unicast messaging."""
     connected_clients.add(websocket)
@@ -19,10 +20,12 @@ async def connection_handler(websocket):
     finally:
         connected_clients.remove(websocket)
 
+
 async def main():
     """Starts the WebSocket server."""
     async with websockets.serve(connection_handler, "localhost", 8765):
         await asyncio.Future()  # Run forever
+
 
 if __name__ == "__main__":
     asyncio.run(main())
